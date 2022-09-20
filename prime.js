abae717fad17d78af9e4d9424933d2d4821e74f2
Suraj barole <you@example.com>
2022-09-20 12:28:17 +0530

    done here

--- prime.js
+++ prime.js
@@ -1,20 +1,21 @@
-function prime(num){
+function prime(number){
 let count=0;
-for(let i=0;i<=num;i++
-if(num%i==0){
+for(let i=0;i<=number;i++){
+if(number%i==0){
 count++;
-}
-}
+    }
+   }
 if(count==2){
-return true;
-}return false;
+    return true;
+} 
+    return false;
 }
 
 
-let answer=prime(13);
-if(answer==true){
-console.log("Prime");
-}else{
-console.log("Not Prime")
+    let answer=prime(23);
+   if(answer==true){
+     console.log("Prime");
+}   else{
+     console.log("Not Prime")
 }
 
